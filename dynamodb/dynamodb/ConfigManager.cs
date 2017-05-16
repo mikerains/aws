c08303f2bd6ce13f055bc2abca04c16229dc2510
Rains, Michael <Michael_Rains@gallup.com>
2017-05-16 11:47:38 -0500

    rought draft

---
+++ dynamodb/dynamodb/ConfigManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Newtonsoft.Json;
+
+namespace dynamodb
+{
+    public class ConfigManager
+    {
+        /// <summary>
+        /// Use ambient credentials and config
+        /// </summary>
+        private AmazonDynamoDBClient adbclient = new AmazonDynamoDBClient();
+
+        /// <summary>
+        /// Config table
+        /// </summary>
+        public const string TABLENAME = "APPCONFIG";
+
+        public const string PARTITION_KEY = "ProjectName";
+
+        public const string SORT_KEY = "ConfigurationKey";
+
+        private Dictionary<string, object> fakeCache = new Dictionary<string, object>();
+
+
+        public T Get<T>(string ProjectName, string ConfigurationKey)
+            where T : class, new()
+        {
+            object obj = GetFromCache(ProjectName, ConfigurationKey);
+            if (null != obj)
+            {
+                return obj as T;
+            }
+            else
+            {
+                LoadCache(ProjectName);
+            }
+
+            obj = GetFromCache(ProjectName, ConfigurationKey);
+            if (null != obj)
+            {
+                return obj as T;
+            }
+            else
+            {
+                throw new Exception($"{ProjectName}:{ConfigurationKey} Does Not Exist");
+            }
+        }
+
+        private object GetFromCache(string projectname, string key)
+        {
+            if (this.fakeCache.Keys.Contains($"{projectname}:{key}"))
+            {
+                return this.fakeCache[$"{projectname}:{key}"];
+            } else
+            {
+                return null;
+            }
+        }
+
+        private void LoadCache(string ProjectName)
+        {
+            // http://docs.aws.amazon.com/amazondynamodb/latest/gettingstartedguide/GettingStarted.NET.04.html
+            //see the example All movies released in 1985
+            Table table = GetTableObject(adbclient, TABLENAME);
+            Search search = table.Query(ProjectName, new Expression());
+            List<Document> docList = new List<Document>();
+            do
+            {
+                docList = search.GetNextSet();
+                foreach (var doc in docList)
+                {
+                    Type type = Type.GetType(doc["ConfigurationType"]);
+                    var obj = JsonConvert.DeserializeObject(doc["Value"], type);
+                    fakeCache.Add($"{ProjectName}:" + doc["ConfigurationKey"], obj);
+                }
+            } while (!search.IsDone);
+        }
+
+
+        // -------------------
+        private Table GetTableObject(AmazonDynamoDBClient client, string tableName)
+        {
+            Table table = null;
+            try
+            {
+                table = Table.LoadTable(client, tableName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n Error: failed to load the 'Movies' table; " + ex.Message);
+                return (null);
+            }
+            return (table);
+        }
+    }
+}
---
+++ dynamodb/dynamodb/TableManager.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+
+namespace dynamodb
+{
+    /// <summary>
+    /// This would not be deployed to production. 
+    /// Used by prototype to create table and document the schema being investigated.
+    /// </summary>
+    public class TableManager
+    {
+
+        private static AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        /// <summary>
+        /// This would not be deployed to production. 
+        /// Used by prototype to create table and document the schema being investigated.
+        /// </summary>
+        public void CreateConfigTable()
+        {
+            Console.WriteLine("\n*** Creating table ***");
+            var request = new CreateTableRequest
+            {
+                AttributeDefinitions = new List<AttributeDefinition>()
+            {
+                new AttributeDefinition
+                {
+                    AttributeName = ConfigManager.PARTITION_KEY,
+                    AttributeType = "S"
+                },
+                new AttributeDefinition
+                {
+                    AttributeName = ConfigManager.SORT_KEY,
+                    AttributeType = "S"
+                }
+            },
+                KeySchema = new List<KeySchemaElement>
+            {
+                new KeySchemaElement
+                {
+                    AttributeName = ConfigManager.PARTITION_KEY,
+                    KeyType = "HASH" //Partition key
+                },
+                new KeySchemaElement
+                {
+                    AttributeName = ConfigManager.SORT_KEY,
+                    KeyType = "RANGE" //Sort key
+                }
+            },
+                ProvisionedThroughput = new ProvisionedThroughput
+                {
+                    ReadCapacityUnits = 1,
+                    WriteCapacityUnits = 1
+                },
+                TableName = ConfigManager.TABLENAME
+            };
+
+            var response = client.CreateTable(request);
+
+            var tableDescription = response.TableDescription;
+            Console.WriteLine("{1}: {0} \t ReadsPerSec: {2} \t WritesPerSec: {3}",
+                      tableDescription.TableStatus,
+                      tableDescription.TableName,
+                      tableDescription.ProvisionedThroughput.ReadCapacityUnits,
+                      tableDescription.ProvisionedThroughput.WriteCapacityUnits);
+
+            string status = tableDescription.TableStatus;
+            Console.WriteLine(ConfigManager.TABLENAME + " - " + status);
+
+            WaitUntilTableReady(ConfigManager.TABLENAME);
+        }
+
+        private static void WaitUntilTableReady(string tableName)
+        {
+            string status = null;
+            // Let us wait until table is created. Call DescribeTable.
+            do
+            {
+                System.Threading.Thread.Sleep(5000); // Wait 5 seconds.
+                try
+                {
+                    var res = client.DescribeTable(new DescribeTableRequest
+                    {
+                        TableName = tableName
+                    });
+
+                    Console.WriteLine("Table name: {0}, status: {1}",
+                              res.Table.TableName,
+                              res.Table.TableStatus);
+                    status = res.Table.TableStatus;
+                }
+                catch (ResourceNotFoundException)
+                {
+                    // DescribeTable is eventually consistent. So you might
+                    // get resource not found. So we handle the potential exception.
+                }
+            } while (status != "ACTIVE");
+        }
+    }
+}
