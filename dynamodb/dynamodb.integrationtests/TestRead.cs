4a5b57bbe169104ef31806ee622600731263abb4
Rains, Michael <Michael_Rains@gallup.com>
2017-05-15 23:06:06 -0500

    dynamodb initial

---
+++ dynamodb/dynamodb.integrationtests/TestRead.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+
+namespace dynamodb.integrationtests
+{
+    [TestFixture]
+    public class TestRead
+    {
+        private AmazonDynamoDBClient ddbclient = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            ddbclient = new AmazonDynamoDBClient();
+
+        }
+
+
+        [Test]
+        public void TestMethod()
+        {
+            // Arrange
+
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
---
+++ dynamodb/dynamodb/DbService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.DynamoDBv2;
+
+namespace dynamodb
+{
+    public class DbService
+    {
+        public void GetData()
+        {
+            //AmazonDynamoDBConfig config = new AmazonDynamoDBConfig() { RegionEndpoint = RegionEndpoint.USEast2 };
+            //Amazon.Runtime.BasicAWSCredentials cred = new Amazon.Runtime.BasicAWSCredentials("", "");
+            //AmazonDynamoDBClient client = new AmazonDynamoDBClient(cred, config);
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+
+        }
+    }
+}
